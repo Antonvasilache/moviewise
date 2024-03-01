3bc2b58c09dc478243cefb522148557198a40166
Varthag <vasilacheanton91@gmail.com>
2024-03-01 16:53:15 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  Box,
+  ErrorMessage,
+  Loader,
+  Main,
+  MovieDetails,
+  MovieList,
+  NavBar,
+  NumResults,
+  Search,
+  WatchedMoviesList,
+  WatchedSummary,
+} from "./components/index";
+import { useMovies, useLocalStorageState } from "./hooks/index";
 
-function App() {
+export const KEY = `2610c182`;
+
+export const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [selectedId, setSelectedId] = useState(null);
+  const { movies, isLoading, error } = useMovies(query);
+  const [watched, setWatched] = useLocalStorageState([], "watched");
+
+  function handleSelectMovie(id) {
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
+  }
+
+  function handleCloseMovie() {
+    setSelectedId(null);
+  }
+
+  function handleAddWatched(movie) {
+    const found = watched.find(
+      (prevMovie) => movie.imdbID === prevMovie.imdbID
+    );
+
+    if (!found) setWatched((watched) => [...watched, movie]);
+  }
+
+  function handleDeleteWatched(id) {
+    setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar>
+        <Search query={query} setQuery={setQuery} />
+        <NumResults movies={movies} />
+      </NavBar>
+
+      <Main>
+        <Box>
+          {isLoading && <Loader />}
+          {!isLoading && !error && (
+            <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+          )}
+          {error && <ErrorMessage message={error} />}
+        </Box>
+        <Box>
+          {selectedId ? (
+            <MovieDetails
+              selectedId={selectedId}
+              onCloseMovie={handleCloseMovie}
+              onAddWatched={handleAddWatched}
+              watched={watched}
+            />
+          ) : (
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList
+                watched={watched}
+                onDeletedWatched={handleDeleteWatched}
+              />
+            </>
+          )}
+        </Box>
+      </Main>
+    </>
   );
 }
-
-export default App;
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+export { Box } from "./Box";
+export { Logo } from "./Logo";
+export { Main } from "./Main";
+export { Movie } from "./Movie";
+export { Search } from "./Search";
+export { Loader } from "./Loader";
+export { NavBar } from "./NavBar";
+export { MovieList } from "./MovieList";
+export { NumResults } from "./NumResults";
+export { ErrorMessage } from "./ErrorMessage";
+export { MovieDetails } from "./MovieDetails";
+export { WatchedMovie } from "./WatchedMovie";
+export { WatchedSummary } from "./WatchedSummary";
+export { default as StarRating } from "./StarRating";
+export { WatchedMoviesList } from "./WatchedMoviesList";
